ca466459eb04a0e4f64df21f767144753e3566f5
Mitchel <mitchel_joy@hotmail.com>
2024-09-13 01:48:32 -0230

    init and started home

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef(null);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const handleClickOutside = (e) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <header>
+            <h1>Mitchel Joy</h1>
+            <div
+                onClick={toggleDropdown}
+                className={`dropdown ${isOpen ? 'show' : ''}`}
+                ref={dropdownRef}
+            >
+                <span id="menuText">
+                    Menu
+                </span>
+                <ul className="dropdown-menu">
+                    <li className="dropdown-item" id="Link1">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="dropdown-item" id="Link2">
+                        <Link to="/projects">Projects</Link>
+                    </li>
+                    <li className="dropdown-item" id="Link3">
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div id="Home">
+            <div className="Banner">
+                <div id="BannerText">
+                    <div>
+                        <h2 id="Front-End">Front-End <br />Developer</h2><div id="BigSlash"></div> <h2 id="UI-UX"> UI/UX <br/> designer</h2>
+                    </div>
+                    <p>Turning Concepts into Visually Stunning and Functional Designs</p>
+                </div>
+                <div id="BannerImg">
+
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Home;
